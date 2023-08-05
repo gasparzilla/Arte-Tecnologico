98177d75a3c1be8390c0f58eccbeefb57ee81e8a
oyster <oyster@multivacDebian.phi>
2023-08-05 15:55:46 -0400

    mic nuevo

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Material Arte Tecnologico 2023
 
-En este repositorio encontrarán los codigos correspondientes a las respuestas de los desafios propuestos en clase
+En este repositorio encontrarán los codigos correspondientes a las respuestas de los desafios propuestos en clase para el segundo semestre del 2023.
+
+Las sesiones están divididas por medio artistico, con la intencion de presentar componentes que apunten a distintos objetivos artisticos.
+
+## Sesion 2: Arte Luminico
+
+Componentes:
+- 
+
+## Sesion 3: 
+
+## Sesion 4: Arte Sonoro
 
 # TODO: TERMINAR README
---
+++ Sesion2/Codigos/Micro_analogo/Micro_analogo.ino
@@ -0,0 +1,55 @@
+#include "FastLED.h"
+
+#define N_LEDS 18
+CRGB leds[N_LEDS];
+#define pinLed 6
+
+// Definir el pin a utilizar por el microfono
+#define pinMic A0
+
+int volumen = 0;
+
+void setup(){
+  FastLED.addLeds<WS2811, pinLed, GRB>(leds, N_LEDS).setCorrection( TypicalLEDStrip );
+  Serial.begin(9600);
+}
+
+void loop(){
+  // Leer el valor del microfono digital
+  int microfono = analogRead(pinMic);
+  Serial.println(microfono);
+  int encenderLeds = map(microfono, 450, 850, 0, 15);
+  if (encenderLeds < 0 ){
+    encenderLeds = 0;
+  }
+  // Encender los LEDs dependiendo del volumen
+  for (int i = 0; i < encenderLeds; i++){
+    // Si los leds encendidos son los primeros 10
+    // encender en verde
+    if (i<10){
+      cambiar_LED(i, 0, 175, 0);
+    }
+    // Si los leds encendidos son del 11 al 13
+    // encender en amarillo
+    else if (i<13){
+      cambiar_LED(i, 125, 125, 0);
+    }
+    // Si los leds encendidos son del 14 al 15
+    // Encender en Rojo
+    else {
+      cambiar_LED(i, 175, 0, 0);
+    }
+  }
+  // Apagar los LEDs dependiendo del volumen
+  for (int i = encenderLeds-1; i < 15; i++){
+    cambiar_LED(i, 0, 0, 0);
+    }
+  // Mostrar los leds encendidos en la tira led
+  FastLED.show();
+}
+
+void cambiar_LED(int pixel, int rojo, int verde, int azul){
+  leds[pixel].r = rojo;
+  leds[pixel].g = verde;
+  leds[pixel].b = azul;
+}
